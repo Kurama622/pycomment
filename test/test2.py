4b046a63ef253fb3427f2c9a145b83d5dd545ae9
demonlord1997 <2273893094@qq.com>
2021-02-04 00:17:21 +0800

    add test2.py

---
+++ test/test2.py
@@ -0,0 +1,12 @@
+def testdef1(a):
+    return a
+
+
+class testClass:
+    def testfunc1(self, a:int, b:float) -> int:
+        a = 100
+        b = float(a)
+        return a
+    def testfunc2(self, a:int=100, b:float=1.0):
+        print()
+
