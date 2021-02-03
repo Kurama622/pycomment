07bf4e37cd197173eac1fe541a4d61b6f01d427f
demonlord1997 <2273893094@qq.com>
2021-02-04 00:20:12 +0800

    add test2.py

--- test/test2.py
+++ test/test2.py
@@ -1,12 +1,57 @@
 def testdef1(a):
+    """testdef1. 
+
+    Parameters
+    ----------
+
+    a. <++>
+
+    Returns
+    -------
+
+    a. <++>
+
+    """
     return a
 
 
 class testClass:
+    """testClass. 
+
+    """
     def testfunc1(self, a:int, b:float) -> int:
+        """testfunc1. 
+
+        Parameters
+        ----------
+
+        self. <++>
+
+        a : int. <++>
+
+        b : float. <++>
+
+        Returns
+        -------
+
+        a : int. <++>
+
+        """
         a = 100
         b = float(a)
         return a
     def testfunc2(self, a:int=100, b:float=1.0):
-        print()
+        """testfunc2. 
+
+        Parameters
+        ----------
+
+        self. <++>
+
+        a : int. <++>
+
+        b : float. <++>
+
+        """
+        print("")
 
